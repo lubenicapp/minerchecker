242093595f5b0d531aa4ef4a09f4bb9becddf653
Vô Song Nguyen <34004481+TontonPizza@users.noreply.github.com>
2021-09-09 03:23:45 +0200

    Update comet_manager.py

--- comet_manager.py
+++ comet_manager.py
@@ -22,9 +22,3 @@ def get_all_classic_comets_in_sol(sol, type='both'):
     for i in range(len(list_comet_normal)):
         comet_data.append(Comet(list_comet_normal[i]))
     return comet_data
-
-if __name__ == "__main__":
-    list_comet = get_all_classic_comets_in_sol(2)
-    for i in range(len(list_comet)):
-        position = list_comet[i].current_cartesian_position()
-        print("current position = ", position[0], " ", position[1], "", list_comet[i].angle)
